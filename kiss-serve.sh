48e84b4030b0ea4a9e43c0d7ece2f50194e88028
Viktor Petersson <vpetersson@wireload.net>
2012-01-12 13:28:33 +0100

    Added clean-up routine for empty folders. Improved on documentation

--- kiss-serve.sh
+++ kiss-serve.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Where to store the images?
+# Where should we store the images? 
+# !!!! This must be a dedicated folder, otherwise the cleanup process will wipe content. !!!!!
 BASE=/path/to/camdump/
 
-# Days to keep backups for.
+# Interval between recordings (in seconds)
+DELAY=1
+
+# Days to keep the recording archive for.
 DAYSTOKEEP=30
+
 
 CAM1=http://1.2.3.4/axis-cgi/jpg/image.cgi
 CAM2=http://1.2.3.4/axis-cgi/jpg/image.cgi
@@ -18,15 +23,16 @@ case "$1" in
 			mkdir -p $BASE/$DIR
 			wget $CAM1 --quiet --output-document=$BASE/$DIR/$SEC\_cam1.jpg
 			# wget $CAM2 --quiet --output-document=$BASE/$DIR/$SEC\_cam2.jpg
-			sleep 1
+			sleep $DELAY
 		done
 	;;
 	
 	cleanup)
-		# Remove files older than 30 days
+		# Remove files older than DAYSTOKEEP
 		if [ $DAYSTOKEEP -gt 1 ]
 		then
 			find $BASE -type f -mtime +$DAYSTOKEEP -exec rm {} \;
+			find $BASE -type d -empty -depth -exec rmdir {} \;
 		else
 			echo "Days to keep is less than 1 day. Exiting."
 		fi
